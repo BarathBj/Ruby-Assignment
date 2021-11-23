1f1a45e9e374daf77516c1a67b3fb4a293e9809f
BarathBj <94238296+BarathBj@users.noreply.github.com>
2021-11-23 16:07:56 +0530

    Add files via upload

---
+++ ex20_sample_test1.rb
@@ -0,0 +1,38 @@
+input_file = ARGV.first
+
+def print_all(f)
+  puts f.read
+end
+
+def rewind(f)
+  f.seek(0)
+end
+
+def print_a_line(line_count, f)
+f = gets
+f ||= ''
+f.chomp!
+  puts "#{line_count}, #{f.chomp!}"
+end
+
+puts input_file
+current_file = open(input_file)
+
+puts "First let's print the whole file:\n"
+
+print_all(current_file)
+
+puts "Now let's rewind, kind of like a tape."
+
+rewind(current_file)
+
+puts "Let's print three lines:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
